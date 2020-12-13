730932775300206b7e254a5f4d7aec982226342e
Rui Huang <rhua966@aucklanduni.ac.nz>
2020-12-13 22:42:30 +1300

    Initial Commit

---
+++ BookKeeper/BookKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookKeeperApp.swift
+//  BookKeeper
+//
+//  Created by Rui Huang on 13/12/20.
+//
+
+import SwiftUI
+
+@main
+struct BookKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
